0b8e6f650bffe7ce31d436032f61dcea1eabde97
Lam Nguyen <kiminonawa1305@gmail.com>
2024-03-16 13:58:26 +0700

    feat: function check network

--- app/src/main/java/com/lamnguyen/ticket_movie_nlu/View/Activities/GreetingActivity.java
+++ app/src/main/java/com/lamnguyen/ticket_movie_nlu/View/Activities/GreetingActivity.java
@@ -1,21 +1,31 @@
 package com.lamnguyen.ticket_movie_nlu.View.Activities;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.graphics.Color;
 import android.graphics.LinearGradient;
 import android.graphics.Shader;
+import android.net.ConnectivityManager;
 import android.os.Bundle;
 import android.os.CountDownTimer;
 import android.os.Handler;
+import android.text.Spannable;
+import android.text.SpannableString;
+import android.text.style.AbsoluteSizeSpan;
 import android.util.Log;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.lamnguyen.ticket_movie_nlu.R;
 
 public class GreetingActivity extends AppCompatActivity {
     private static final String TAG = "GreetingActivity";
+
+    private AlertDialog alertDialog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,6 +37,8 @@ public class GreetingActivity extends AppCompatActivity {
                 getColor(R.color.purple),
                 getColor(R.color.red)
         };
+
+        buildAlertDialogNetwork();
         float width = tvGreeting.getLayoutParams().width;
         float height = tvGreeting.getLayoutParams().height;
         Shader shader = new LinearGradient(
@@ -36,17 +48,54 @@ public class GreetingActivity extends AppCompatActivity {
         setTimerChangePageSign();
     }
 
+    private void buildAlertDialogNetwork() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(GreetingActivity.this);
+
+        // Tạo một SpannableString từ tiêu đề
+        SpannableString title = new SpannableString(getString(R.string.do_not_connect_to_network));
+
+        // Thiết lập kích thước font cho tiêu đề
+        title.setSpan(new AbsoluteSizeSpan(22, true), 0, title.length(), Spannable.SPAN_INCLUSIVE_INCLUSIVE);
+
+        builder.setTitle(title)
+                .setMessage(getString(R.string.request_connect_to_network))
+                .setIcon(R.drawable.ic_warning)
+                .setPositiveButton(getString(R.string.re_try_check_password), new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        setTimerChangePageSign();
+                    }
+                })
+                .setNegativeButton(getString(R.string.cancel), new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        finish();
+                    }
+                });
+        this.alertDialog = builder.create();
+    }
+
     private void setTimerChangePageSign() {
 
-        new CountDownTimer(1500, 1000) {
+        new CountDownTimer(5000, 1000) {
             public void onTick(long millisUntilFinished) {
+                if (isNetworkConnected()) {
+                    Intent intent = new Intent(GreetingActivity.this, SignActivity.class);
+                    startActivity(intent);
+                    cancel();
+                    finish();
+                }
             }
 
             public void onFinish() {
-                Intent intent = new Intent(GreetingActivity.this, SignActivity.class);
-                startActivity(intent);
-                finish();
+                alertDialog.show();
             }
         }.start();
+    }
+
+    private boolean isNetworkConnected() {
+        ConnectivityManager cm = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+
+        return cm.getActiveNetworkInfo() != null && cm.getActiveNetworkInfo().isConnected();
     }
 }
