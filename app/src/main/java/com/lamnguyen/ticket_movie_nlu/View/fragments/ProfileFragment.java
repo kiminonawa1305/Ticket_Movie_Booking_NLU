a1a9f239482aa17aed33730a67397c038658fe69
Lam Nguyen <kiminonawa1305@gmail.com>
2024-03-17 17:34:24 +0700

    feat: complete function sign out

--- app/src/main/java/com/lamnguyen/ticket_movie_nlu/View/fragments/ProfileFragment.java
+++ app/src/main/java/com/lamnguyen/ticket_movie_nlu/View/fragments/ProfileFragment.java
@@ -1,6 +1,7 @@
 package com.lamnguyen.ticket_movie_nlu.view.fragments;
 
 import android.app.Dialog;
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -10,12 +11,23 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.google.firebase.auth.FirebaseAuth;
 import com.lamnguyen.ticket_movie_nlu.R;
+import com.lamnguyen.ticket_movie_nlu.model.utils.DialogLoading;
+import com.lamnguyen.ticket_movie_nlu.model.utils.ThreadCallBackSign;
+import com.lamnguyen.ticket_movie_nlu.service.UserService.UserService;
+import com.lamnguyen.ticket_movie_nlu.service.UserService.impl.UserServiceImpl;
+import com.lamnguyen.ticket_movie_nlu.service.auth.change_password.ChangePasswordService;
+import com.lamnguyen.ticket_movie_nlu.service.auth.change_password.impl.ChangePasswordServiceImpl;
+import com.lamnguyen.ticket_movie_nlu.view.activities.SignActivity;
 
 public class ProfileFragment extends Fragment {
-    private Button btnChangePassword, btnSetting, btnInformation, btnSignOut;
-    Dialog dlgChangePassword;
+    private Button btnChangePassword, btnSetting, btnInformation, btnSignOut, btnSubmitChangePassword;
+    private TextView edtNewPassword, edtReNewPassword;
+    private Dialog dlgChangePassword;
+    private DialogLoading dialogLoading;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -37,24 +49,85 @@ public class ProfileFragment extends Fragment {
         btnChangePassword = view.findViewById(R.id.button_change_pasword);
         btnSetting = view.findViewById(R.id.button_setting);
         btnSignOut = view.findViewById(R.id.button_sign_out);
+
+
+        dialogLoading = DialogLoading.getInstance(getActivity());
+    }
+
+    private Dialog getInstanceDialogChangePassword() {
+        if (dlgChangePassword == null) {
+            dlgChangePassword = new Dialog(this.getContext());
+            dlgChangePassword.setContentView(R.layout.dialog_change_password);
+            initViewInDialogChangePassword();
+            btnSubmitChangePassword.setOnClickListener(view -> {
+                eventChangePassword();
+            });
+        }
+
+        return dlgChangePassword;
     }
 
     private void event() {
         btnChangePassword.setOnClickListener(v -> {
-            dialogChangePassword();
+            getInstanceDialogChangePassword().show();
+        });
+
+        btnSignOut.setOnClickListener(v -> {
+            eventSignOut();
         });
     }
 
-    private void dialogChangePassword() {
-        dlgChangePassword = new Dialog(this.getContext());
-        dlgChangePassword.setContentView(R.layout.dialog_change_password);
-        dlgChangePassword.show();
+    private void eventSignOut() {
+        FirebaseAuth.getInstance().signOut();
+        Intent intent = new Intent(getActivity(), SignActivity.class);
+        this.startActivity(intent);
+        getActivity().finish();
     }
 
-    private void getPassword() {
-        TextView edtNewPassword = dlgChangePassword.findViewById(R.id.edit_text_new_password);
-        TextView edtReNewPassword = dlgChangePassword.findViewById(R.id.edit_text_re_new_password);
+    private void eventChangePassword() {
+        String password = edtNewPassword.getText().toString();
+        String rePassword = edtNewPassword.getText().toString();
 
+        UserService service = UserServiceImpl.getInstance();
+        int result = service.matchPassword(password, rePassword);
+        switch (result) {
+            case UserService.EMPTY_PASSWORD: {
+                edtNewPassword.setError(getString(R.string.request_password));
+                break;
+            }
+            case UserService.EMPTY_RE_PASSWORD: {
+                edtNewPassword.setError(getString(R.string.request_re_password));
+                break;
+            }
+            case UserService.NOT_MATCH: {
+                edtNewPassword.setError(getString(R.string.not_match_password));
+                edtNewPassword.setError(getString(R.string.not_match_password));
+                break;
+            }
+            case UserService.MATCH: {
+                dialogLoading.showDialogLoading(getString(R.string.sign_up));
+                ChangePasswordService changePasswordService = ChangePasswordServiceImpl.getInstance();
 
+                changePasswordService.changePassword(password, new ThreadCallBackSign() {
+                    @Override
+                    public void isSuccess() {
+                        dialogLoading.dismissDialogLoading();
+                        Toast.makeText(getContext(), getString(R.string.change_password_success), Toast.LENGTH_SHORT).show();
+                    }
+
+                    @Override
+                    public void isFail() {
+                        dialogLoading.dismissDialogLoading();
+                        Toast.makeText(getContext(), getString(R.string.change_password_fail), Toast.LENGTH_SHORT).show();
+                    }
+                });
+            }
+        }
+    }
+
+    private void initViewInDialogChangePassword() {
+        edtNewPassword = dlgChangePassword.findViewById(R.id.edit_text_new_password);
+        edtReNewPassword = dlgChangePassword.findViewById(R.id.edit_text_re_new_password);
+        btnChangePassword = dlgChangePassword.findViewById(R.id.button_submit_change_pasword);
     }
 }
--- app/src/main/java/com/lamnguyen/ticket_movie_nlu/view/activities/SignActivity.java
+++ app/src/main/java/com/lamnguyen/ticket_movie_nlu/view/activities/SignActivity.java
@@ -4,9 +4,12 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.FragmentManager;
 
 import android.app.Dialog;
+import android.content.Intent;
 import android.os.Bundle;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.google.firebase.auth.FirebaseAuth;
 import com.lamnguyen.ticket_movie_nlu.R;
 
 public class SignActivity extends AppCompatActivity {
@@ -18,10 +21,20 @@ public class SignActivity extends AppCompatActivity {
     public static final String EMAIL_PATTERN = "^(?=.{1,64}@)[A-Za-z0-9_-]+(\\.[A-Za-z0-9_-]+)*@[^-][A-Za-z0-9-]+(\\.[A-Za-z0-9-]+)*(\\.[A-Za-z]{2,})$";
 
 
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        if (FirebaseAuth.getInstance().getCurrentUser() != null) {
+            Intent intent = new Intent(this, MainActivity.class);
+            this.startActivity(intent);
+            this.finish();
+        }
     }
 }
