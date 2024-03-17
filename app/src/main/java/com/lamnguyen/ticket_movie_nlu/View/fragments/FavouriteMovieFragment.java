11a9e5968fc524d02021cf503de57b56bfce4a07
Lam Nguyen <kiminonawa1305@gmail.com>
2024-03-17 15:31:11 +0700

    feat: add file FavouriteMovieFragment.java

---
+++ app/src/main/java/com/lamnguyen/ticket_movie_nlu/View/fragments/FavouriteMovieFragment.java
@@ -0,0 +1,26 @@
+cpackage com.lamnguyen.ticket_movie_nlu.view.fragments;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.lamnguyen.ticket_movie_nlu.R;
+
+public class FavouriteMovieFragment extends Fragment {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_favourite_movie, container, false);
+    }
+}
