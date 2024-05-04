8d434742d57173dc9ced3aa4f88e2208fc0db9d3
john9101 <trinhdong1098@gmail.com>
2024-05-04 22:51:50 +0700

    complete code ui introduction page

---
+++ app/src/main/java/com/lamnguyen/ticket_movie_nlu/View/Activities/IntroductionActivity.java
@@ -0,0 +1,16 @@
+package com.lamnguyen.ticket_movie_nlu.view.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.lamnguyen.ticket_movie_nlu.R;
+
+public class IntroductionActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_introduction);
+    }
+}
