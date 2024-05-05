1d045eabcbd566ed129a878ad5419e66ebe4996c
OzuSus <21130327@st.hcmuaf.edu.vn>
2024-05-05 10:24:01 +0700

    save commit

---
+++ app/src/main/java/com/lamnguyen/ticket_movie_nlu/view/activities/FavoriteMovieList.java
@@ -0,0 +1,17 @@
+package com.lamnguyen.ticket_movie_nlu.view.activities;
+
+import android.os.Bundle;
+import android.os.PersistableBundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.lamnguyen.ticket_movie_nlu.R;
+
+public class FavoriteMovieList extends AppCompatActivity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_favorite_movie);
+    }
+}
