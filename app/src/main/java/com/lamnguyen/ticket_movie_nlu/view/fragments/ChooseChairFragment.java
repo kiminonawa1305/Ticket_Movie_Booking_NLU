2ece63e9f52999ad1407e1221480be56a301b52c
Lam Nguyen <kiminonawa1305@gmail.com>
2024-05-08 17:27:38 +0700

    fix:fix Ui

--- app/src/main/java/com/lamnguyen/ticket_movie_nlu/view/fragments/ChooseChairFragment.java
+++ app/src/main/java/com/lamnguyen/ticket_movie_nlu/view/fragments/ChooseChairFragment.java
@@ -4,12 +4,14 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
 import android.widget.GridView;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import com.lamnguyen.ticket_movie_nlu.R;
-import com.lamnguyen.ticket_movie_nlu.adapters.SeatAdapter;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -17,22 +19,24 @@ import java.util.List;
 public class ChooseChairFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        View rootView = inflater.inflate(R.layout.fragment_seat_item_list, container, false);
+        return inflater.inflate(R.layout.fragment_seat_item_list, container, false);
+    }
 
-        GridView gridView = rootView.findViewById(R.id.gridView);
-        List<String> seatList = generateSeatList(); // Hàm để tạo danh sách ghế
-        SeatAdapter adapter = new SeatAdapter(requireContext(), seatList);
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        GridView gridView = view.findViewById(R.id.grid_view_seats);
+        gridView.setNumColumns(16);
+        gridView.getLayoutParams().width = 16 * 100;
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this.getContext(), R.layout.item_seat, generateSeatList());
         gridView.setAdapter(adapter);
-
-        return rootView;
     }
 
     private List<String> generateSeatList() {
         // Tạo danh sách ghế dựa trên ma trận ghế trong rạp chiếu phim
         // Ví dụ: ["A1", "A2", "A3", ...]
         List<String> seatList = new ArrayList<>();
-        for (char row = 'A'; row <= 'K'; row++) {
+        for (char row = 'A'; row <= 'Z'; row++) {
             for (int col = 1; col <= 12; col++) {
                 seatList.add(String.valueOf(row) + col);
             }
