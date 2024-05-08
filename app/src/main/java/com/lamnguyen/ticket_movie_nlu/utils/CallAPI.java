a6ec500b29601c7139e4e18819f29c28f9799715
Lam Nguyen <kiminonawa1305@gmail.com>
2024-05-08 14:09:54 +0700

    fix: fix time out

--- app/src/main/java/com/lamnguyen/ticket_movie_nlu/utils/CallAPI.java
+++ app/src/main/java/com/lamnguyen/ticket_movie_nlu/utils/CallAPI.java
@@ -1,8 +1,10 @@
 package com.lamnguyen.ticket_movie_nlu.utils;
 
 import android.content.Context;
+import android.util.Log;
 
 import com.android.volley.AuthFailureError;
+import com.android.volley.DefaultRetryPolicy;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.toolbox.JsonObjectRequest;
@@ -16,14 +18,19 @@ import java.util.Map;
 import lombok.SneakyThrows;
 
 public class CallAPI {
-    public static final String URL_WEB_SERVICE = "http://10.0.23.94:80";
+    public static final String URL_WEB_SERVICE = "http://10.0.23.200:8080";
     public static final String URL_OMDB = "http://www.omdbapi.com/?apikey=c3d0a99f";
     public static final String URL_GOOGLE_MAP_COMPUTE_ROUTES = "https://routes.googleapis.com/directions/v2:computeRoutes";
     public static final String URL_GOOGLE_MAP_DIRECTION = "https://maps.googleapis.com/maps/api/directions/json";
+    private static final int TIME_OUT = 1000 * 60 * 10;
 
     public static void callStringRequest(Context context, String url, String query, int method, Response.Listener<String> responseListener, Response.ErrorListener errorListener) {
         RequestQueue queue = Volley.newRequestQueue(context);
         StringRequest stringRequest = new StringRequest(method, url + query, responseListener, errorListener);
+        stringRequest.setRetryPolicy(new DefaultRetryPolicy(
+                TIME_OUT,
+                DefaultRetryPolicy.DEFAULT_MAX_RETRIES,
+                DefaultRetryPolicy.DEFAULT_BACKOFF_MULT));
         //Set TAG
         stringRequest.setTag(context.getClass().getSimpleName());
         // Add the request to the RequestQueue.
@@ -33,11 +40,16 @@ public class CallAPI {
     @SneakyThrows
     public static void callJsonObjectRequest(Context context, String url, String query, int method, Response.Listener<JSONObject> responseListener, Response.ErrorListener errorListener) {
         RequestQueue queue = Volley.newRequestQueue(context);
-        JsonObjectRequest stringRequest = new JsonObjectRequest(method, url + query, null, responseListener, errorListener);
+        Log.i("URL", url + query);
+        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(method, url + query, null, responseListener, errorListener);
+        jsonObjectRequest.setRetryPolicy(new DefaultRetryPolicy(
+                TIME_OUT,
+                DefaultRetryPolicy.DEFAULT_MAX_RETRIES,
+                DefaultRetryPolicy.DEFAULT_BACKOFF_MULT));
         //Set TAG
-        stringRequest.setTag(context.getClass().getSimpleName());
+        jsonObjectRequest.setTag(context.getClass().getSimpleName());
         // Add the request to the RequestQueue.
-        queue.add(stringRequest);
+        queue.add(jsonObjectRequest);
     }
 
     public static void callStringRequest(Context context, String url, String query, String body, Map<String, String> header, int method, Response.Listener<String> responseListener, Response.ErrorListener errorListener) {
@@ -53,6 +65,10 @@ public class CallAPI {
                 return body.getBytes();
             }
         };
+        stringRequest.setRetryPolicy(new DefaultRetryPolicy(
+                TIME_OUT,
+                DefaultRetryPolicy.DEFAULT_MAX_RETRIES,
+                DefaultRetryPolicy.DEFAULT_BACKOFF_MULT));
         //Set TAG
         stringRequest.setTag(context.getClass().getSimpleName());
         // Add the request to the RequestQueue.
@@ -61,7 +77,7 @@ public class CallAPI {
 
     public static void callJsonObjectRequest(Context context, String url, String query, String body, Map<String, String> header, int method, Response.Listener<JSONObject> responseListener, Response.ErrorListener errorListener) {
         RequestQueue queue = Volley.newRequestQueue(context);
-        JsonObjectRequest stringRequest = new JsonObjectRequest(method, url + query, null, responseListener, errorListener) {
+        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(method, url + query, null, responseListener, errorListener) {
             @Override
             public Map<String, String> getHeaders() throws AuthFailureError {
                 Map<String, String> headers = super.getHeaders();
@@ -74,10 +90,14 @@ public class CallAPI {
                 return body.getBytes();
             }
         };
+        jsonObjectRequest.setRetryPolicy(new DefaultRetryPolicy(
+                TIME_OUT,
+                DefaultRetryPolicy.DEFAULT_MAX_RETRIES,
+                DefaultRetryPolicy.DEFAULT_BACKOFF_MULT));
         //Set TAG
-        stringRequest.setTag(context.getClass().getSimpleName());
+        jsonObjectRequest.setTag(context.getClass().getSimpleName());
         // Add the request to the RequestQueue.
-        queue.add(stringRequest);
+        queue.add(jsonObjectRequest);
     }
 
     public interface CallAPIListener<T> {
