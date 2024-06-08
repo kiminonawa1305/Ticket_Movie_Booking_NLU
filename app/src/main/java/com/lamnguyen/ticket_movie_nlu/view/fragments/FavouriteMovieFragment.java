a440ec601b988dd07166771cb47c0f2f018e8bef
lamnguyen <kiminonawa1305@gmail.com>
2024-06-08 19:56:13 +0700

    clear code

--- app/src/main/java/com/lamnguyen/ticket_movie_nlu/view/fragments/FavouriteMovieFragment.java
+++ app/src/main/java/com/lamnguyen/ticket_movie_nlu/view/fragments/FavouriteMovieFragment.java
@@ -2,6 +2,11 @@ package com.lamnguyen.ticket_movie_nlu.view.fragments;
 
 import android.app.Dialog;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -9,25 +14,15 @@ import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
-import android.util.Log;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.Toast;
-
 import com.lamnguyen.ticket_movie_nlu.R;
 import com.lamnguyen.ticket_movie_nlu.adapters.MovieAdapter;
 import com.lamnguyen.ticket_movie_nlu.api.MovieApi;
 import com.lamnguyen.ticket_movie_nlu.dto.MovieDTO;
-import com.lamnguyen.ticket_movie_nlu.dto.MovieDetailDTO;
-import com.lamnguyen.ticket_movie_nlu.dto.UserDTO;
 import com.lamnguyen.ticket_movie_nlu.utils.CallAPI;
 import com.lamnguyen.ticket_movie_nlu.utils.DialogLoading;
 
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 public class FavouriteMovieFragment extends Fragment {
     private RecyclerView rvDisplayFavoriteMovie;
