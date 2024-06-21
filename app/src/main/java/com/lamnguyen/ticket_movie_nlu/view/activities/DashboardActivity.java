e8c7664ff3b8a181292526c7ec6bcbe1ed1f7e7a
lamhongphong <21130473@st.hcmuaf.edu.vn>
2024-06-21 14:07:01 +0700

    ui: finish base

--- app/src/main/java/com/lamnguyen/ticket_movie_nlu/view/activities/DashboardActivity.java
+++ app/src/main/java/com/lamnguyen/ticket_movie_nlu/view/activities/DashboardActivity.java
@@ -1,6 +1,10 @@
 package com.lamnguyen.ticket_movie_nlu.view.activities;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -10,6 +14,7 @@ import com.anychart.chart.common.dataentry.DataEntry;
 import com.anychart.chart.common.dataentry.ValueDataEntry;
 import com.anychart.charts.Cartesian;
 import com.anychart.charts.Pie;
+import com.anychart.core.cartesian.series.Line;
 import com.anychart.enums.Anchor;
 import com.anychart.enums.Position;
 import com.anychart.enums.TooltipPositionMode;
@@ -21,50 +26,49 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class DashboardActivity extends AppCompatActivity {
+    private static final String TAG = "DashboardActivity";
+    private Button btnDay, btnWeek, btnMonth;
+    private ImageView imgViewEditCalendar;
+    private AnyChartView anyChartViewDisplayNumOfSaleTicket, getAnyChartViewDisplayRevenueReport;
+    private TextView txtViewDay, txtViewWeek, txtViewMonth, txtViewSumOfSaleTicket, txtViewSumOfRevenue;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dashboard);
 
-//        AnyChartView anyChartView = findViewById(R.id.any_chart_view);
+        btnDay = findViewById(R.id.button_day);
+        btnWeek = findViewById(R.id.button_week);
+        imgViewEditCalendar = findViewById(R.id.image_view_edit_calendar);
+        anyChartViewDisplayNumOfSaleTicket = findViewById(R.id.any_chart_view_display_sale_ticket);
+        getAnyChartViewDisplayRevenueReport = findViewById(R.id.any_chart_view_display_revenu_report);
+        txtViewDay = findViewById(R.id.text_view_show_day);
+        txtViewWeek = findViewById(R.id.text_view_show_week);
+        txtViewMonth = findViewById(R.id.text_view_show_month);
+        txtViewSumOfSaleTicket = findViewById(R.id.text_view_sum_sale_ticket);
+        txtViewSumOfRevenue = findViewById(R.id.text_view_sum_revenu);
 
+        createLineChart();
+    }
+
+    private void createLineChart() {
+        Log.i(TAG, "createLineChart: Run");
         Cartesian cartesian = AnyChart.line();
 
-        cartesian.animation(true);
-
-        cartesian.padding(10d, 20d, 5d, 20d);
-
-        cartesian.crosshair().enabled(true);
-        cartesian.crosshair()
-                .yLabel(true)
-                .yStroke((Stroke) null, null, null, (String) null, (String) null);
-
-        cartesian.tooltip().positionMode(TooltipPositionMode.POINT);
-
-        cartesian.title("Trend of Sales of the Most Popular Products of ACME Corp.");
-
-        cartesian.yAxis(0).title("Number of Bottles Sold (thousands)");
-        cartesian.xAxis(0).labels().padding(5d, 5d, 5d, 5d);
-
         List<DataEntry> seriesData = new ArrayList<>();
-        seriesData.add(new ValueDataEntry("1986", 3.6));
-        seriesData.add(new ValueDataEntry("1987", 7.1));
-        seriesData.add(new ValueDataEntry("1988", 8.5));
-        seriesData.add(new ValueDataEntry("1989", 9.2));
-        seriesData.add(new ValueDataEntry("1990", 10.1));
-        seriesData.add(new ValueDataEntry("1991", 11.6));
-        seriesData.add(new ValueDataEntry("1992", 16.4));
-        seriesData.add(new ValueDataEntry("1993", 18.0));
-        seriesData.add(new ValueDataEntry("1994", 13.2));
-        seriesData.add(new ValueDataEntry("1995", 12.0));
+        seriesData.add(new ValueDataEntry("Jan", 10000));
+        seriesData.add(new ValueDataEntry("Feb", 12000));
+        seriesData.add(new ValueDataEntry("Mar", 18000));
+        seriesData.add(new ValueDataEntry("Apr", 11000));
+        seriesData.add(new ValueDataEntry("May", 15000));
+        seriesData.add(new ValueDataEntry("Jun", 13000));
 
-        cartesian.line(seriesData).name("Brandy").tooltip()
-                .position(Position.RIGHT_CENTER)
-                .anchor(Anchor.LEFT_CENTER)
-                .offsetX(5d)
-                .offsetY(5d);
+        Line series = cartesian.line(seriesData);
+        series.name("Sales");
 
-//        anyChartView.setChart(cartesian);
+        cartesian.title("Monthly Sales Data");
+
+        getAnyChartViewDisplayRevenueReport.setChart(cartesian);
     }
 }
