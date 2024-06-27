42eeea5ebd5ff81cdb767c5efaf58eecaef88d7c
OzuSus <21130327@st.hcmuaf.edu.vn>
2024-06-27 16:47:47 +0700

    callJson Array

--- app/src/main/java/com/lamnguyen/ticket_movie_nlu/utils/CallAPI.java
+++ app/src/main/java/com/lamnguyen/ticket_movie_nlu/utils/CallAPI.java
@@ -7,11 +7,13 @@ import com.android.volley.AuthFailureError;
 import com.android.volley.DefaultRetryPolicy;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
+import com.android.volley.toolbox.JsonArrayRequest;
 import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
 import com.google.gson.JsonObject;
 
+import org.json.JSONArray;
 import org.json.JSONObject;
 
 import java.util.Map;
@@ -20,7 +22,7 @@ import lombok.SneakyThrows;
 
 public class CallAPI {
 
-    public static final String IP = "172.16.1.246";
+    public static final String IP = "172.16.1.163";
     public static final String URL_WEB_SERVICE = "http://" + IP + ":8080";
     public static final String URL_OMDB = "http://www.omdbapi.com/?apikey=c3d0a99f";
     public static final String URL_GOOGLE_MAP_COMPUTE_ROUTES = "https://routes.googleapis.com/directions/v2:computeRoutes";
@@ -102,6 +104,19 @@ public class CallAPI {
         jsonObjectRequest.setTag(context.getClass().getSimpleName());
         // Add the request to the RequestQueue.
         queue.add(jsonObjectRequest);
+    }
+
+    @SneakyThrows
+    public static void callJsonArrayRequest(Context context, String url, String query, int method, Response.Listener<JSONArray> responseListener, Response.ErrorListener errorListener) {
+        RequestQueue queue = Volley.newRequestQueue(context);
+        Log.i(CallAPI.class.getSimpleName(), url + query);
+        JsonArrayRequest jsonArrayRequest = new JsonArrayRequest(method, url + (query == null ? "" : query), null, responseListener, errorListener);
+        jsonArrayRequest.setRetryPolicy(new DefaultRetryPolicy(
+                TIME_OUT,
+                DefaultRetryPolicy.DEFAULT_MAX_RETRIES,
+                DefaultRetryPolicy.DEFAULT_BACKOFF_MULT));
+        jsonArrayRequest.setTag(context.getClass().getSimpleName());
+        queue.add(jsonArrayRequest);
     }
 
     public interface CallAPIListener<T> {
