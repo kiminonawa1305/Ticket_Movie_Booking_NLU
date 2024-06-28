d68c4a1b350448954a6ac32331fff45ddc87f1b2
john9101 <21130319@st.hcmuaf.edu.vn>
2024-06-28 19:33:46 +0700

    remove loadMovies in MovieService

--- app/src/main/java/com/lamnguyen/ticket_movie_nlu/service/movie/MovieService.java
+++ app/src/main/java/com/lamnguyen/ticket_movie_nlu/service/movie/MovieService.java
@@ -29,10 +29,6 @@ public class MovieService {
         movieApi.loadMovie(date, context, listener);
     }
 
-//    public void loadMovies(Context context, CallAPI.CallAPIListener<List<MovieDTO>> listener){
-//        movieApi.loadMovies(context, listener);
-//    }
-
 
     public void loadShowtime(int movieId, LocalDate date, Context context, CallAPI.CallAPIListener<List<ShowtimeByCinema>> listener) {
         movieApi.loadShowtime(movieId, date, context, listener);
